7a6cc6ee17d424d567c21452dcdba1abaa464ca1
Corneezius <cfr2116@columbia.edu>
2018-06-26 13:54:25 -0400

    refactor component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
-
-
 import SearchBar from "./components/search_bar";
-
 const API_KEY = "AIzaSyDUJhjfzq-80986_smoW27BTmZl_N-PR8s";
 
-YTSearch({ key: API_KEY, term: "surfboards"}, function(data) {
-  console.log(data);
-})
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return <div>
-  <SearchBar />
-  </div>;
+    this.state = { videos: []};
+
+    YTSearch({ key: API_KEY, term: "surfboards"}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
